fcc21e9d0704921e742fa998cecc5ff7123cf1de
cassia cristina <c.cassiacristina21@gmail.com>
2023-12-10 00:44:28 -0300

    stickers atualizados

--- my_bot_gm.py
+++ my_bot_gm.py
@@ -15,13 +15,6 @@ lista_respostas = ["Zm Zcasher","Zm zfriend","gm Zfriend", "gm Zcasher"]
 
 lista_respostas_n = ["Zn Zcasher","Zn zfriend","gn Zfriend", "gn Zcasher"]
 
-lista_imagens = [ ]
-
-directory = './imagem/' 
-for filename in os.listdir(directory):
-        lista_imagens.append(filename)
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
@@ -35,31 +28,30 @@ async def on_message(message):
 
     if message.author == client.user :
      return 
+    
+    lista_stk = await message.guild.fetch_stickers()
+    random_sticker = random.choice(lista_stk)
 
     for cmd in lista_comandos:
      if unidecode(cmd.lower().replace(" ", "")) in unidecode(message.content.lower().replace(" ", "")):
         await message.add_reaction ('☀️') 
         await message.add_reaction("<:zcash:1060629265961472080>")
-        imagem = random.choice(lista_imagens)
         resposta = random.choice(lista_respostas)
-        await message.reply(content=resposta, file=discord.File(f'./imagem/{imagem}'))
+        await message.reply(content=resposta,stickers=[random_sticker])
         break
      
     for cmdn in lista_comandos_n:
         if unidecode(message.content).replace(" ", "").lower().startswith(unidecode(cmdn).replace(" ", "").lower()):
             await message.add_reaction ('🌘') 
             await message.add_reaction("<:zcash:1060629265961472080>")
-            imagem = random.choice(lista_imagens)
             resposta = random.choice(lista_respostas_n)
-            await message.reply(content=resposta, file=discord.File(f'./imagem/{imagem}'))
+            await message.reply(content=resposta,stickers=[random_sticker])
             break
-
-    if message.content.lower() == "zm":
+        
+    if unidecode(message.content).replace(" ", "").lower().startswith("zm"):
       await message.add_reaction ('☕')
 
-    if message.content.lower() == "zn":
+    if unidecode(message.content).replace(" ", "").lower().startswith("zn"):
       await message.add_reaction ('🍵')
-
-
 
 client.run(my_token)       
